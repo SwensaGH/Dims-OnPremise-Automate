78748bddba341162f90789a7fa3944d72745b22c
ankaiahjaladi <ankaiah@swensa.com>
2025-06-25 17:26:43 +0530

    add  customer pisd

--- schema/entrypoint.sh
+++ schema/entrypoint.sh
@@ -39,9 +39,7 @@ fi
 
 echo 'FLUSH PRIVILEGES ;' >> "$TEMP_FILE"
 
-echo "before dims.sql"
-#cat /dims.sql >> "$TEMP_FILE"
-echo "after dims.sql"
+
 
 ## ------------------------------------------------------------
 ## Fill the isd_calendar table
